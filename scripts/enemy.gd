9dee5e566f499876c0b554fb60fc117697b5c183
JoshuaCranitch <joshua.cranitch@gmail.com>
2026-05-08 22:27:32 +1000

    Added Enemy Automatic Spawning
    Enemies now spawn offscreen at a random position

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
 
-var enemySpeed = 300.0
+var enemySpeed = 150.0
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
