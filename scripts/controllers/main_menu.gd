d16e9e6ede725b25b614cef087052165bde5f036
JoshuaCranitch <joshua.cranitch@gmail.com>
2026-05-10 08:22:06 +1000

    Bird makes noise. Buttons make noise. Quit Button works

--- scripts/controllers/main_menu.gd
+++ scripts/controllers/main_menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var button_type = null
 @onready var ping_animation = $PingAnimation
+@onready var click_sound = $ClickSound
 
 func _ready() -> void:
 	ping_animation.play("idle")
@@ -15,16 +16,28 @@ func _process(_delta):
 		get_tree().change_scene_to_file("res://scenes/mainScenes/tutorial.tscn")
 	
 	elif button_type == "quit":
-		print("quit game")
+		get_tree().quit()
 
 func _on_start_pressed() -> void:
+	click_sound.play()
 	button_type = "start"
 
-func _on_options_pressed() -> void:
-	button_type = "options"
-
 func _on_quit_pressed() -> void:
+	click_sound.play()
 	button_type = "quit"
 
 func _on_tutorial_pressed() -> void:
+	click_sound.play()
 	button_type = "tutorial"
+
+
+func _on_start_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_tutorial_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_mouse_entered() -> void:
+	pass # Replace with function body.
