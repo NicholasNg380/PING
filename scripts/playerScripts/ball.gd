b4d90342c10ca7559575dbe99aa9095d436934c9
Nicholas Ng <nnyc380@outlook.com>
2026-05-09 22:39:34 +1000

    Ball to new main
    Ball damage was re-added to new main

--- scripts/playerScripts/ball.gd
+++ scripts/playerScripts/ball.gd
@@ -1,12 +1,9 @@
 class_name Ball extends Node2D
 
+@onready var player = get_node("/root/Game/Player")
 
-const SPEED: int = 700
-const RETURN_SPEED: int = 850
-const WALL_RETURN_SPEED: int = 1000
 enum State {INACTIVE, HIT_ENEMY, HIT_PADDLE, HIT_WALL}
 var ball_state = State.INACTIVE
-var player
 signal has_ball
 signal can_parry
 signal cannot_parry
@@ -14,8 +11,27 @@ signal cannot_parry
 var sprite
 var direction
 
+'''
+Player Hold Variables
+'''
+var SPEED: int 
+var SPEED_MULTIPLIER: int
+var RETURN_SPEED: int
+var WALL_RETURN_SPEED: int
+var RETURN_SPEED_MULTIPLIER: float
+var DAMAGE: float
+var DAMAGE_MULTIPLIER: float
+var RETURN_DAMAGE_MULTIPLIER: float
+
 func _ready() -> void:
-	player = get_parent()
+	SPEED = player.ball_speed
+	SPEED_MULTIPLIER = player.ball_return_speed_multi
+	RETURN_SPEED = player.ball_return_speed
+	WALL_RETURN_SPEED = player.ball_return_wall_speed
+	RETURN_SPEED_MULTIPLIER = player.ball_return_speed_multi
+	DAMAGE = player.ball_damage
+	DAMAGE_MULTIPLIER = player.ball_damage_multi
+	RETURN_DAMAGE_MULTIPLIER = player.ball_return_damage_multi
 	global_position = player.global_position
 	sprite = $BallSpin
 	set_as_top_level(true)
@@ -27,13 +43,13 @@ func _process(delta: float) -> void:
 		global_position = player.global_position
 	elif ball_state == State.HIT_PADDLE:
 		sprite.show()
-		global_position += direction * SPEED * delta
+		global_position += direction * SPEED * SPEED_MULTIPLIER * delta
 	elif ball_state == State.HIT_ENEMY:
 		direction = global_position.direction_to(player.global_position)
-		global_position += direction * RETURN_SPEED * delta
+		global_position += direction * RETURN_SPEED * RETURN_SPEED_MULTIPLIER * delta
 	else:
 		direction = global_position.direction_to(player.global_position)
-		global_position += direction * WALL_RETURN_SPEED * delta
+		global_position += direction * WALL_RETURN_SPEED * RETURN_SPEED_MULTIPLIER * delta
 
 func hit_paddle():
 	ball_state = State.HIT_PADDLE
@@ -43,8 +59,10 @@ func _on_ball_hit_box_body_entered(body: Node2D) -> void:
 	if ball_state == State.HIT_PADDLE:
 		if body.is_in_group("Enemy"):
 			ball_state = State.HIT_ENEMY
-			body.take_damage()
+			body.take_damage(DAMAGE * DAMAGE_MULTIPLIER)
 	if ball_state == State.HIT_ENEMY or ball_state == State.HIT_WALL:
+		if body.is_in_group("Enemy"):
+			body.take_damage(DAMAGE * RETURN_DAMAGE_MULTIPLIER)
 		if body.is_in_group("Player"):
 			ball_state = State.INACTIVE
 			has_ball.emit()
