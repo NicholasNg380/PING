fe7c8eb9db1b39781c608cd0de7f6db73ab893a8
JoshuaCranitch <joshua.cranitch@gmail.com>
2026-05-10 10:43:38 +1000

    Added Crossfade with menu music and game music

--- scripts/controllers/main_menu.gd
+++ scripts/controllers/main_menu.gd
@@ -4,8 +4,11 @@ var button_type = null
 @onready var ping_animation = $PingAnimation
 @onready var click_sound = $ClickSound
 @onready var hover_sound = $HoverSound
+@onready var menu_music = preload("res://assets/sound/menuMusic.wav")
+@onready var game_music = preload("res://assets/sound/backmusic.ogg")
 
 func _ready() -> void:
+	MusicManager.play_music(menu_music, 0.8)
 	get_tree().paused = false
 	ping_animation.play("idle")
 	pass
